f00b11740ef6f1a4febaa8ee7f50b37bbaafa8a0
ReVaN <revannn5480@gmail.com>
2020-10-06 14:21:47 +0200

    Add routing

---
+++ src/app.php
@@ -0,0 +1,23 @@
+<?php
+
+use Symfony\Component\Routing;
+
+$routes = new Routing\RouteCollection();
+$routes->add(
+    'hello',
+    new Routing\Route('/hello/{name}',
+        [
+            'name' => 'World',
+            '_controller' => 'render_template'
+        ])
+);
+
+$routes->add(
+    'bye',
+    new Routing\Route('/bye',
+        [
+            '_controller' => 'render_template'
+        ])
+);
+
+return $routes;
--- web/front.php
+++ web/front.php
@@ -4,23 +4,31 @@ require_once __DIR__ . '/../vendor/autoload.php';
 
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing;
+
+function render_template(Request $request) {
+    extract($request->attributes->all(), EXTR_SKIP);
+    ob_start();
+    include sprintf(__DIR__ . '/../src/pages/%s.php', $_route);
+
+    return new Response(ob_get_clean());
+}
 
 $request = Request::createFromGlobals();
+$routes = include __DIR__ . '/../src/app.php';
 
-$map = [
-  '/hello' => 'hello',
-  '/bye'   => 'bye',
-];
+$context = new Routing\RequestContext();
+$context->fromRequest($request);
+$matcher = new Routing\Matcher\UrlMatcher($routes, $context);
 
-$path = $request->getPathInfo();
+try {
+    $request->attributes->add($matcher->match($request->getPathInfo()));
 
-if (isset($map[$path])) {
-    ob_start();
-    extract($request->query->all(), EXTR_SKIP);
-    include sprintf(__DIR__ . '/../src/pages/%s.php', $map[$path]);
-    $response = new Response(ob_get_clean());
-} else {
+    $response = call_user_func($request->attributes->get('_controller'), $request);
+} catch (Routing\Exception\ResourceNotFoundException $e) {
     $response = new Response(Response::$statusTexts[404], Response::HTTP_NOT_FOUND);
+} catch (Exception $e) {
+    $response = new Response(Response::$statusTexts[500], Response::HTTP_INTERNAL_SERVER_ERROR);
 }
 
 $response->send();
